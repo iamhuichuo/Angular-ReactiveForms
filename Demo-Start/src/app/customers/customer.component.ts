f1e8d6509f0d134b94fb30e51680b62e8d1e52cd
iamhuichuo <69033539+iamhuichuo@users.noreply.github.com>
2023-09-04 17:40:05 +0800

    update

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 import { Customer } from './customer';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -9,14 +9,31 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    });
+  }
 
   save(): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData(): void {
+    this.customerForm.setValue({
+      firstName: 'Jack',
+      lastName: 'Wong',
+      email: 'jack@mail.com',
+      sendCatalog: false,
+    });
   }
 }
